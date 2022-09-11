013924af0a570f6b724b820c1ff2b4257e882953
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-09-11 18:15:10 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+ #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  /******** declare model parameters ******/
+  // hard coded parameters
+  int c0;     // number of initial cases
+  double r;   //growth rate factor
+  double m;   // power of polynomial
+
+  // calculated values based on equation 1 and 2
+  double ct;     // c(t)
+  double A;      // equation constant related to baseline prevalence
+
+  // user input
+  double t;      // weeks
+
+  /********* initialize model parameters *******/
+  c0 = 10;
+  r = 0.2;
+  m = 1.65;
+  // t = 10;
+
+  /******** input *******************************/
+  // request user input
+  cout << "For the Model where m = " << m << ", c0 = " << c0;
+  cout << ", and r = " << r << "..." << endl;
+  cout << "Input t [weeks] for the sub-exponential growth model" << endl;
+  cin >> t;
+
+  /********* Conversions and Calculations *****************/
+  // convert t from weeks to days
+  t = t*7;
+
+  
+
+  
+}
